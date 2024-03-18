549c24659686478f97b75706d66557717d618b0f
OX2Github <mojtaba.jabbari.ghadi@ox2.com>
2024-03-18 12:29:25 +1100

    Restructuring

--- BESS/main_script.py
+++ BESS/main_script.py
@@ -14,88 +14,21 @@ import time
 import pandas                              as pd
 import dispatch_optimiser
 import Import_Inputs
+import scenario
 from   dispatch_optimiser import dispatch_optimiser
 from   generator          import Generator as gen
 from   scenario           import Scenario  as scn  
-from datetime             import datetime, timedelta
-
-#========================================================================================
-# ============== Calculate the 1st day of the next month ================================ 
-def first_day_of_next_month(date_str):
-    # Assuming the date is in the format "dd/mm/yyyy"
-    day, month, year = map(int, date_str.split('/'))
-
-    # Get the 1st day of the next month
-    month = month + 1
-    if month == 13: 
-        month = 1
-        year += 1
-    return dt.datetime(year, month, day).strftime("%d/%m/%Y")
-# ========================================================================================
-def divide_period(start_date, end_date):
-    # Parse start_date and end_date strings to datetime objects
-    start_date = datetime.strptime(start_date, "%d/%m/%Y")
-    end_date   = datetime.strptime(end_date  , "%d/%m/%Y")
-    
-    # Initialize a list to store timestamps
-    timestamps = []
-
-    # Start iterating from start_date until end_date
-    current_date = start_date
-    while current_date <= end_date:
-        # Determine the end of the current month
-        end_of_month = current_date.replace(day=1) + timedelta(days=32)
-        end_of_month = end_of_month.replace(day=1) - timedelta(days=1)
-        
-        # Adjust the end_of_month if it goes beyond the end_date
-        if end_of_month > end_date:
-            end_of_month = end_date
-        
-        # Add timestamp for the current month
-        timestamps.append((current_date.strftime("%d/%m/%Y"), (end_of_month+ timedelta(days=1)).strftime("%d/%m/%Y")))
-        # timestamps.append((current_date.strftime("%d/%m/%Y"), (end_of_month).strftime("%d/%m/%Y")))
-        # Move to the next month
-        current_date = end_of_month + timedelta(days=1)
-    
-    return timestamps
-
-#========================================================================================
-# ============== Format month and year ================================================== 
-def format_date(month, year):
-    return dt.datetime(year, month, 1).strftime("%d/%m/%Y")
-
-#========================================================================================
-# ============== Creat directory for output folder======================================= 
-def create_folder(directory):
-
-    # Check if the parent folder "Output" already exists, otherwise Create it
-    parent_directory = os.path.dirname(directory)
-    if not os.path.exists(parent_directory):
-        os.mkdir(parent_directory)
-
-    # Check if the folder already exists, otherwise Create a new folder
-    if not os.path.exists(directory       ):
-        os.mkdir(directory       )
-
 
 #========================================================================================
 # ============== Parallel computation: Distributing computations on several cores ======= 
 def run_script_multiprocessing(Inputs):
-    # Get number of cores available: 
-    # num_cores = multiprocessing.cpu_count()
-
     Multi_Processing= Inputs['Multi-Processing']
 
     # Example usage
     start_date = Inputs['start_timestamp'] # "2023-01-5"
-    end_date   = Inputs['end_timestamp'] #  "2023-05-7"
-    TimeStamps = divide_period(start_date, end_date)
-
-        
-
-
-    # month_year_combinations = [(month, year) for year in years   for month in months]
-
+    end_date   = Inputs['end_timestamp'  ] #  "2023-05-7"
+    TimeStamps = scenario.divide_period(start_date, end_date)
+      
     if Multi_Processing:
         #  Multiprocessing
         num_cores = multiprocessing.cpu_count()
@@ -115,16 +48,6 @@ def run_script_multiprocessing(Inputs):
 #========================================================================================
 # ============== Main solver of the algorithm =========================================== 
 def main_solve(start_date, end_date, Inputs):   
-    # start_date = format_date            (month, year) 
-    # end_date   = first_day_of_next_month(start_date )
-    
-    # ------------- refer the script to the relevant documents --------------------
-    # username = '341510anla' # Change this parameter to the username of the computer profile this script is being run on. sim mac: Esco-nas01, Angus PC: 10.0.0.210
-    # path=r"C:\Users\{}\OneDrive - OX2\Data\18. Grid\BESS Model Remote\Basic BESS Inputs".format(username)+r"\\"
-    # project_information = path + "Project information_template.xlsx"
-
-    #  ------ Get input parameters from Project information.xlsx -------------------   
-    # Inputs = Import_Inputs. Import_Data(folder_path)
 
     #  ------ Get input plant/scenario/solver parameters ---------------------------   
     plant_parameters={
@@ -189,20 +112,8 @@ def main_solve(start_date, end_date, Inputs):
 
 #========================================================================================
 # ============== Main algorithm body ==================================================== 
-r"""
-start_time = time.time()
-folder_path = r"C:\Users\341510anla\OneDrive - OX2\Data\18. Grid\BESS Model Remote\Basic BESS Inputs\Output\\" + time.strftime("%B-%d %H_%M", time.localtime(start_time)) +"_45MW_2hr_results"
-create_folder(folder_path)
-main_solve(4, 2025,folder_path)
-"""
+
 if __name__ == "__main__":
-    # months = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
-    # # duration of opt in years
-    # dur   = 3
-    # years = [] 
-    # # Add the desired range of years
-    # for i in range(0,dur):
-    #     years.append(2025+i)
         
     Inputs = Import_Inputs. Import_Data()
     
@@ -213,8 +124,7 @@ if __name__ == "__main__":
     Output_SubFolder  = time . strftime("%B-%d %H_%M", time. localtime(start_time)) +" "+Inputs['Scenario Name']
     OutputFolder_path = os   . path. join (parent_directory, OutPut_directory, Output_SubFolder)
     Inputs['output_directory'] = OutputFolder_path
-
-    create_folder(OutputFolder_path)
+    scenario.create_folder(OutputFolder_path)
    
     df = run_script_multiprocessing(Inputs)
     #df.to_csv(folder_path+r"\\concatenated_results.csv",index=False)
@@ -222,5 +132,3 @@ if __name__ == "__main__":
     execution_time = end_time - start_time
     print(f"The execution time: {execution_time}")
 
-
-#"""
--- BESS/scenario.py
+++ BESS/scenario.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 """
 Created on Mon Feb 27 17:36:08 2023
-
 @ Author: SIMULATION
 @ Revised/restructured: Mojtaba Jabbari Ghadi
-
 """
+from    datetime   import datetime, timedelta
+import  os
+
 class Scenario:
     def __init__(self, scenario_params):
         self.start_timestamp            = scenario_params['start_timestamp'             ]
@@ -36,3 +37,44 @@ class Scenario:
         print("data_source:              ", self.data_source            )
         print("export_limits:            ", self.export_limits          )
 
+#========================================================================================
+# ================== Divide simulation period into monthly sub-periods ==================
+def divide_period(start_date, end_date):
+    # Parse start_date and end_date strings to datetime objects
+    start_date = datetime.strptime(start_date, "%d/%m/%Y")
+    end_date   = datetime.strptime(end_date  , "%d/%m/%Y")
+    
+    # Initialize a list to store timestamps
+    timestamps = []
+
+    # Start iterating from start_date until end_date
+    current_date = start_date
+    while current_date <= end_date:
+        # Determine the end of the current month
+        end_of_month = current_date.replace(day=1) + timedelta(days=32)
+        end_of_month = end_of_month.replace(day=1) - timedelta(days=1)
+        
+        # Adjust the end_of_month if it goes beyond the end_date
+        if end_of_month > end_date:
+            end_of_month = end_date
+        
+        # Add timestamp for the current month
+        timestamps.append((current_date.strftime("%d/%m/%Y"), (end_of_month+ timedelta(days=1)).strftime("%d/%m/%Y")))
+        # timestamps.append((current_date.strftime("%d/%m/%Y"), (end_of_month).strftime("%d/%m/%Y")))
+        # Move to the next month
+        current_date = end_of_month + timedelta(days=1)
+    return timestamps
+
+
+#========================================================================================
+# ============== Creat directory for output folder======================================= 
+def create_folder(directory):
+
+    # Check if the parent folder "Output" already exists, otherwise Create it
+    parent_directory = os.path.dirname(directory)
+    if not os.path.exists(parent_directory):
+        os.mkdir(parent_directory)
+
+    # Check if the folder already exists, otherwise Create a new folder
+    if not os.path.exists(directory       ):
+        os.mkdir(directory       )
